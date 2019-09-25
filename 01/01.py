f35703651738f0607116629b37f4c544ff2a7c56
Carol Wang <carolwang1117@gmail.com>
2019-09-25 16:21:22 +0800

    modify week 1 code to program1 and add in program2

--- 01/01.py
+++ 01/01.py
@@ -1,5 +1,10 @@
 #program 1
-a = input('What is your name?')
-print('Hello,', a)
+ask_name = input('What is your name?')
+print('Hello,',ask_name + '!')
 
+#program 2
+question = input('What temperature do you want in Celsius?')
+Celsius = float(question)
+Fahrenheit = Celsius*1.8+32
+print(Fahrenheit)
 
--- 02/02.py
+++
@@ -1,5 +0,0 @@
-#program 2
-question = input('What temperature do you want in Celsius?')
-Celsius = float(question)
-Fahrenheit = Celsius*1.8+32
-print(Fahrenheit)
