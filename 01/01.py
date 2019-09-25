f3026c1050c2d0a304627e0dd84a1ed8827ff91a
Carol Wang <carolwang1117@gmail.com>
2019-09-25 15:35:43 +0800

    modify 01, add 02 and exercise

--- 01/01.py
+++ 01/01.py
@@ -1,5 +1,5 @@
-#%%
-a = 'Hello, world!'
-print(a)
+#program 1
+a = input('What is your name?')
+print('Hello,', a)
 
-#%%
+
---
+++ 02/02.py
@@ -0,0 +1,5 @@
+#program 2
+question = input('What temperature do you want in Celsius?')
+Celsius = float(question)
+Fahrenheit = Celsius*1.8+32
+print(Fahrenheit)
