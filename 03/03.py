181ee1ab18880cfad0bd56265664bc88c43c2afc
Carol Wang <carolwang1117@gmail.com>
2019-09-28 00:04:45 +0800

    upload week 3 assignment

---
+++ 03/03.py
@@ -0,0 +1,53 @@
+def hello_w():
+    print('Hello, world!')
+def ask_n():
+    ask_name = input('What is your name?')
+    print('Hello,',ask_name + '!')
+def ask_temp():
+    question = input('What temperature do you want in Celsius?')
+    Celsius = float(question)
+    Fahrenheit = Celsius*1.8+32
+    print(Fahrenheit)
+def count_down():
+    n=5
+    while n>0:
+        print(n)
+        n=n-1
+    print('you died!')
+
+
+while True:
+    line = input('Hello Brave Warroior you made it which door do u want to choose?1, 2 or 3?')
+    if line == '1':
+        hello_w()
+        ask_stop = input('Do u wanna to choose again?Yes or No?')
+        if ask_stop == 'Yes':
+            continue  
+        if ask_stop =='No':
+            break
+        else:
+            print('Wrong Choice!')
+            count_down()
+    if line == '2':
+        ask_n()
+        ask_stop = input('Do u wanna to choose again?Yes or No?')
+        if ask_stop == 'No':
+            break
+        elif ask_stop == 'Yes':
+            continue
+        else:
+            print('Wrong Choice!')
+            count_down()
+    if line == '3':
+        ask_temp()
+        ask_stop = input('Do u wanna to choose again?Yes or No?')
+        if ask_stop == 'Yes':
+            continue
+        if ask_stop == 'No':
+            break
+        else:
+            print('Wrong Choice!')
+            count_down()
+    else:
+        print("There is no other doors. Please choose 1, 2 or 3")
+print('Ok Bye!')
