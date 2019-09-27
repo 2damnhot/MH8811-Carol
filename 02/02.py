fc0eb5b16cd998a959a1f655cef287fbd41bc357
Carol Wang <carolwang1117@gmail.com>
2019-09-27 16:52:45 +0800

    week 1 assignment upload to 01.py

---
+++ 02/02.py
@@ -0,0 +1,9 @@
+#program 1
+ask_name = input('What is your name?')
+print('Hello,',ask_name + '!')
+
+#program 2
+question = input('What temperature do you want in Celsius?')
+Celsius = float(question)
+Fahrenheit = Celsius*1.8+32
+print(Fahrenheit)
