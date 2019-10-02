2fca776d343fa1a720e717b12b8016eeaa88861d
Carol Wang <carolwang1117@gmail.com>
2019-10-02 20:36:54 +0800

    Modify 03.py

--- 03/03.py
+++ 03/03.py
@@ -1,13 +1,17 @@
 def hello_w():
     print('Hello, world!')
+
 def ask_n():
     ask_name = input('What is your name?')
     print('Hello,',ask_name + '!')
+
 def ask_temp():
     question = input('What temperature do you want in Celsius?')
     Celsius = float(question)
     Fahrenheit = Celsius*1.8+32
     print(Fahrenheit)
+    
+#what else can make a function? count down from 5 to 1  
 def count_down():
     n=5
     while n>0:
@@ -15,9 +19,12 @@ def count_down():
         n=n-1
     print('you died!')
 
+"------------------------------------------------------------------------------------------"
 
 while True:
-    line = input('Hello Brave Warroior you made it which door do u want to choose?1, 2 or 3?')
+    print('Hello Brave Warroior you made it which door do u want to choose?')
+    print('1: greeting the world\n2: greeting you\n3: convert temperature from Celsius to Fahrenheit')
+    line = input('>')
     if line == '1':
         hello_w()
         ask_stop = input('Do u wanna to choose again?Yes or No?')
